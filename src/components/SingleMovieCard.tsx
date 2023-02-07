c48a9f18be1a4488bca0b740dace80d258cf724f
Tayra Cutri <tayracutri@gmail.com>
2023-02-06 21:20:03 -0300

    Cast and Genres style fixed

--- src/components/SingleMovieCard.tsx
+++ src/components/SingleMovieCard.tsx
@@ -1,111 +1,79 @@
-import { useLocation } from "react-router-dom"
-import { Result } from "../interfaces/interfaces";
-import { Navbar } from "./Navbar"
+import { useLocation } from "react-router-dom";
+import { Result, Cast } from "../interfaces/interfaces";
+import { Navbar } from "./Navbar";
 
-import '../styles/single-movie.css'
+import "../styles/single-movie.css";
 import { useMovieCast } from "../hooks/useMovieCast";
 import { filterGenresById } from "../helpers/filterGenresById";
 
-export const SingleMovieCard = () => { 
+export const SingleMovieCard = () => {
+  const moviesState = useLocation().state as Result;
+  const { cast } = useMovieCast(moviesState.id);
 
-   const moviesState = useLocation().state as Result;
-   const {cast} = useMovieCast(moviesState.id);
+  //   map the cast: "one, two and three"
+  const newArray = cast.slice(0, 5);
+  const castArray = [] as string[];
 
-   const newArray = cast.slice(0, 5);
+  newArray.forEach((c) => castArray.push(c.name));
+  const lastC = castArray.pop();
 
-   const movieGenres = filterGenresById(moviesState.genre_ids)
-   const n = [] as string[];
-   movieGenres.forEach(g => n.push(g.name));
-   console.log(n);
+  const castResult = castArray.join(", ") + " and " + lastC;
 
-    return(
-        <div style={{
-            width: '100%'
-        }}>
-        <Navbar />
+  // map the genres: 'one, two and three'
+  const movieGenres = filterGenresById(moviesState.genre_ids);
+  const n = [] as string[];
 
-        <div id="single-movie-container"
-            style={{
-                backgroundImage: `linear-gradient(
+  movieGenres.forEach((g) => n.push(g.name));
+
+  const last = n.pop();
+  const genreResult = n.join(", ") + " and " + last;
+
+  return (
+    <div
+      style={{
+        width: "100%",
+      }}
+    >
+      <div
+        id="single-movie-container"
+        style={{
+          backgroundImage: `linear-gradient(
                     to top,
                     hsl(0 0% 0% / 0),
                     hsl(20 0% 0% / 0.3) 40%,
                     hsl(0 0% 0% / 1)
-                ),url(https://image.tmdb.org/t/p/original/${moviesState.backdrop_path})`
-            }}
-        >
-            <div className="single-movie-content">
-                <div className="movie-title">
-                    <span id="original-title"> {moviesState.original_title}</span>
-                    <span className="release-date">({moviesState.release_date.slice(0, 4)})</span>
-                </div>
-               
-                <div className="movie-info">
-                    <p>{moviesState.overview}</p>
-                </div>
-            </div>
+                ),url(https://image.tmdb.org/t/p/original/${moviesState.backdrop_path})`,
+        }}
+      >
+        <div className="left-column">
+          <div className="movie-title">
+            <span id="original-title"> {moviesState.original_title}</span>
+            <span className="release-date">
+              ({moviesState.release_date.slice(0, 4)})
+            </span>
+          </div>
 
-            
-               <div className="cast">
-                <span className="cast-preview">Cast: </span>
-                {
-                    newArray.map((act, i, row) => {
-                        if (i + 1 !== row.length) {                     
-                            return (
-                            <span key={act.id} className='cast-name'>
-                                {act.name}, 
-                            </span>
-                            )
-                        }else{
-                            return (
-                                <span key={act.id} className='cast-name'>
-                                {act.name}.
-                            </span>
-                            )
-                        }
-                    })
-                }
-               
-               
-               <div className="cast">
-                <span className="cast-preview">Genres: </span>
-                {
-
-                    n.map((genre, i, row )=>{
-                            if (i + 1 !== row.length) {                     
-                            return (
-                            <span key={genre} className='cast-name'>
-                                {genre}
-                            </span>
-                            )
-                        }else{
-                            return (
-                                <span key={genre} className='cast-name'>
-                                {genre}.
-                            </span>
-                            )
-                        }
-                    })
-                    // n.map((act, i, row) => {
-                    //     if (i + 1 !== row.length) {                     
-                    //         return (
-                    //         <span key={act.name} className='cast-name'>
-                    //             {act.name}, 
-                    //         </span>
-                    //         )
-                    //     }else{
-                    //         return (
-                    //             <span key={act.id} className='cast-name'>
-                    //             {act.name}.
-                    //         </span>
-                    //         )
-                    //     }
-                    // })
-                }
-               </div>
-               </div>
-            </div>
-            
+          <div className="overview">
+            <p>{moviesState.overview}</p>
+          </div>
         </div>
-    )
- }
+
+        <div className="right-column">
+          <div>
+            <span className="info-preview">Cast: </span>
+            <span className="info-name">{castResult}</span>
+          </div>
+
+          <div
+            style={{
+              marginTop: "1.2rem",
+            }}
+          >
+            <span className="info-preview">Genres: </span>
+            <span className="info-name">{genreResult}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
